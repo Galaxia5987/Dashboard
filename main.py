b56e4b8be408e85373158a3f6bd83213fe6a88f9
eitan <eitan.vichik@gmail.com>
2023-09-07 11:16:35 +0300

    Making progress

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import PySimpleGUI as sg
 
 
-if __name__ == '__main__':
-    print('Hey')
+def your_mom():
+    print("hey")
+
+
+button = sg.Button(button_text="Hey", button_type=9)
+window = sg.Window("Your mom", layout=[[button]], margins=(400, 200))
+
+while True:
+    event, values = window.read()
+
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        break
+
+    print(event)
+
+window.close()
+
+
 
